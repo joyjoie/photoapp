8164c8e3e661a8c99090ce07c77ea368cdeddab2
joy <wendokingoo@gmail.com>
2019-03-03 22:13:57 +0300

    urls file added and modified

---
+++ gallery/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns=[
+   url(r'^$',views.index,name='index'),
+   url(r'^search/', views.search_results, name='search_results'),
+   url(r'^image/(\d+)',views.image,name ='image'),
+   url(r'^location/(\d+)',views.display_img,name ='image')
+
+]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
