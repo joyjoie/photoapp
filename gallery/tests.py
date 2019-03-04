d3ed05e2a12f10a0ef38f6ecb84d7293a807e08f
joy <wendokingoo@gmail.com>
2019-03-04 07:08:29 +0300

    Test  modified

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
-
+import datetime as dt
 # Create your tests here.
+from .models import Editor,Image,category,Location
+
+
+class EditorTestClass(TestCase):
+    
+    # Set up method
+    def setUp(self):
+        self.joy= Editor(first_name = 'Joy', last_name ='W', email ='joy@moringaschool.com')
+
+     # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.joy,Editor))
+
+        
+    # Testing Save Method
+    def test_save_method(self):
+        self.joy.save_editor()
+        editors = Editor.objects.all()
+        self.assertTrue(len(editors) > 0)
+
