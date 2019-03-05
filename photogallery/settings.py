d93056614dcf9bbe1ffe873361d0d7de59eaeea7
joy <wendokingoo@gmail.com>
2019-03-05 08:03:01 +0300

    deployment to heroku

--- photogallery/settings.py
+++ photogallery/settings.py
@@ -53,7 +53,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1+5$x(yke#v!!u-o*ign=ouekcmpigvmby91oi+i)#2$-siy14'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
